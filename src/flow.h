b43385f7a5e6cb794012d011ce6207ed3efe2424
duyhustvn <duy.dinhle.hust.vn@gmail.com>
2026-07-07 22:53:58 +0700

    add method

--- src/flow.h
+++ src/flow.h
@@ -2,34 +2,35 @@
 #define FLOW_H
 
 #include "uthash.h"
+#include <stdint.h>
 
 typedef struct {
-    uint8_t ip_version;
-    uint32_t src_ip;
-    uint32_t dst_ip;
-    uint16_t src_port;
-    uint16_t dst_port;
+  uint8_t ip_version;
+  uint32_t src_ip;
+  uint32_t dst_ip;
+  uint16_t src_port;
+  uint16_t dst_port;
 } FlowKey;
 
 typedef struct {
-    FlowKey key;
+  FlowKey key;
 
-    uint8_t *buffer; 
-    size_t buffer_len;
-    uint32_t next_seq;
+  uint8_t *buffer;
+  size_t buffer_len;
+  uint32_t next_seq;
 
-    uint64_t created_at_ms;
-    uint64_t updated_at_ms;
+  uint64_t created_at_ms;
+  uint64_t updated_at_ms;
 
-    UT_hash_handle hh;
+  UT_hash_handle hh;
 } Flow;
 
-void construct_key(
-    FlowKey *key, 
-    uint8_t ip_version, 
-    uint32_t src_ip, 
-    uint32_t dst_ip, 
-    uint16_t src_port, 
-    uint16_t dst_port);
+void construct_key(FlowKey *key, uint8_t ip_version, uint32_t src_ip,
+                   uint32_t dst_ip, uint16_t src_port, uint16_t dst_port);
+
+FlowKey *lookup(FlowKey *key, Flow *flows);
+
+void upsert(FlowKey *key, Flow *flows, uint8_t *data, size_t data_len,
+            uint32_t sequence_number);
 
 #endif // FLOW_H
